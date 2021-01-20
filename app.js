14b786c3f7da98e6c1bb4785903ffdf663bb88c4
Glitch (hello-express) <none>
2021-01-20 10:04:38 +0000

    🍞💄 Checkpoint ./app.js:26767453/1249

--- app.js
+++ app.js
@@ -4,15 +4,33 @@ const client = new Discord.Client();
 
 client.login(process.env.TOKEN)
 
+var prefix = ','
+
+/////////////////////////////////////////////////////////////////////////////
+
 client.on('message', message => {
   
   if(message.content === 'halo') {
     message.channel.send('haii')
-    
   }
   
-  client.user.setActivity(`ndak bisa basa enggress `, {type : 'WATCHING'});
+  if(message.content === 'kamu lagi ngapain?') {
+    message.channel.send('kan aku lagi main sama ahsan <3')
+  }
   
+//////////////////////////////////////////////////////////////////////////////
   
+  client.user.setActivity(`with ahsan <3 `, {type : 'PLAYING'});
+  
+/////////////////////////////////////////////////////////////////////////////
+  
+  if(message.content.startsWith(`${prefix}help`)) {
+  const ngentot = new Discord.MessageEmbed()
+    .setColor('#945A1B')
+    .setAuthor('Help Command', client.user.displayAvatarURL())
+    .setDescription('ini commands nya sayang')
+    .setFooter('Made with love by Axann#6643');
+  message.channel.send(ngentot)
+}
   
 })
