397faa52b8f1fdcf60bb5077600cfd137a6458dd
Glitch (hello-express) <none>
2021-01-20 09:07:25 +0000

    🎸🏠 Checkpoint ./app.js:26767453/259 ./package.json:26767453/15

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const Discord = require('discord.js')
-const Client = new Discord
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
 client.login(process.env.TOKEN)
+
+client.on('message', message => {
+  
+  if(message.content === 'halo') {
+    message.channel.send('haii')
+    
+  }
+  
+  
+  
+  
+  
+}
